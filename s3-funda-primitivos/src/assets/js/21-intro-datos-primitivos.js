c7e059f1206c01e9c5cb1bcf20119525eff3e036
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2023-09-05 09:43:57 -0500

    Datos primitivos

--- s3-funda-primitivos/src/assets/js/21-intro-datos-primitivos.js
+++ s3-funda-primitivos/src/assets/js/21-intro-datos-primitivos.js
@@ -18,4 +18,32 @@ console.log(typeof nombre);
 /**
  * @TYPE  NUMBER
  */
+nombre = 123;
+console.log( typeof nombre );
 
+
+let esMarvel = false;
+console.log( typeof esMarvel );
+
+
+let edad = 33;
+console.log( typeof edad );
+
+edad = 33.001;
+console.log( typeof edad );
+
+
+let superPoder;
+console.log( typeof superPoder ); // ???
+
+let soyNull = null;
+console.log( typeof soyNull ); // ???
+
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( typeof symbol1 );
+
+
+console.log( symbol1 === symbol2 );
