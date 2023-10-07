194c50a13b678f6fd02d72de5aff966752ac2c07
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2023-10-07 13:07:50 -0500

    feat: valor referencia

---
+++ s4-estrutura-control-flujo/seccion-4.md
@@ -0,0 +1,4 @@
+# Ciclos y estructuras de control
+## Valor, referencia y romper referencia
+> **Código:**
+> [valor-referencia.js](/s4-estrutura-control-flujo/src/js/33-valor-referencia.js).
---
+++ s4-estrutura-control-flujo/src/js/33-valor-referencia.js
@@ -0,0 +1,66 @@
+// js todos los objetos son pasados por referencia
+// todos los primitivos se pasan por valor.
+
+let a = 10;
+let b = a;
+a = 30;
+console.log({ a, b })
+
+//
+
+let juan = { nombre: 'Juan' };
+let ana = juan;
+ana.nombre = 'Ana';
+
+console.log({ juan, ana })
+
+//
+
+const cambiaNombre = (persona) => {
+  persona.nombre = 'Tony';
+  return persona;
+};
+
+let peter = { nombre: 'Peter' };
+let tony = cambiaNombre(peter); // mandamos por referencia
+
+console.log({ peter, tony })
+
+/**
+ * Crear copia de un objeto
+ * Spreat Operator
+ */
+let maria = { nombre: 'Maria' };
+let bryam = { ...maria }; // separa elementos, y rompe la referencia
+bryam.nombre = 'Bryam';
+
+console.log({ maria, bryam })
+
+const cambiaNombre2 = ({ ...persona }) => {
+  persona.nombre = 'Tony';
+  return persona;
+};
+
+let andrea = { nombre: 'Andrea' };
+let tony2 = cambiaNombre2(andrea);
+console.log(andrea, tony2);
+
+
+// ARREGLOS
+const frutas = ['Manzana', 'Pera', 'Piña'];
+
+const otrasFrutas = frutas;
+otrasFrutas.push('Mango');
+console.table({ frutas, otrasFrutas })
+
+console.time('slice');
+const otrasFrutas3 = frutas.slice();
+otrasFrutas3.push('Mango');
+console.table({ otrasFrutas3, frutas })
+console.timeEnd('slice');
+
+console.time('spread');
+const otrasFrutas2 = [...frutas];
+otrasFrutas2.push('Mango');
+console.table({ frutas, otrasFrutas2 });
+console.timeEnd('spread');
